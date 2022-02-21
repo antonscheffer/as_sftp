eda59d4173d8a5847561916b7423ca1c8f435ffc
Lee Lindley <lee.lindley@gmail.com>
2022-02-21 06:44:14 -0500

    add example implementation of methods that can share ssh logins with other schemas

---
+++ src/not_installing_keymgmt_security.sql
@@ -0,0 +1,3 @@
+prompt not installing DBMS_RLS access control for table as_sftp_private_keys
+prompt either because it was not requested or DBMS_RLS was not granted.
+prompt if going to use it, GRANT EXECUTE ON DBMS_RLS TO public;
